204c2163acf6204e37ddafea988b114e0e377bb8
iceqiw <qqwei1123@163.com>
2017-11-07 15:04:20 +0800

    gateway

--- api-gateway-with-eureka/src/main/java/org/ice/api/gateway/apigatewaywitheureka/ApiGatewayWithEurekaApplication.java
+++ api-gateway-with-eureka/src/main/java/org/ice/api/gateway/apigatewaywitheureka/ApiGatewayWithEurekaApplication.java
@@ -1,9 +1,11 @@
 package org.ice.api.gateway.apigatewaywitheureka;
 
+import org.ice.api.gateway.apigatewaywitheureka.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,5 +14,10 @@ public class ApiGatewayWithEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayWithEurekaApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter simpleFilter() {
+		return new AccessFilter();
 	}
 }
---
+++ api-gateway-with-eureka/src/test/java/org/ice/api/gateway/apigatewaywitheureka/ApiGatewayWithEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ice.api.gateway.apigatewaywitheureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiGatewayWithEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
